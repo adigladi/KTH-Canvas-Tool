84a4298e2dac3c2073ef270f4e0f0fed78334d51
adigladi <adigladi@hotmail.com>
2019-11-20 14:48:03 +0100

    adds more data

--- src/components/course/groups.js
+++ src/components/course/groups.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import { Radar } from "react-chartjs-2";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
-import students from "./students"
+import students from "./students";
 
 export default class GroupManagement extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      week: 49
+      week: 49,
+      data: []
     };
   }
 
   componentDidMount() {
-      this.getGroupHours("A")
+    this.setState({ data: this.getGroupData() });
   }
 
   onSliderChange = value => {
@@ -24,31 +25,67 @@ export default class GroupManagement extends Component {
 
   getGroupData = () => {
     var dataset = [];
+    var groupID = ["A", "B", "C", "D"];
+    var colors = [
+      "54, 162, 235",
+      "255, 206, 86",
+      "75, 192, 192",
+      "153, 102, 255"
+    ];
+
+    var groups = [];
+    groupID.forEach(id =>
+      groups.push({ name: "Group " + id, weekData: this.getGroupWeeks(id) })
+    );
+    groups.forEach((group, i) => {
+      dataset.push({
+        label: group.name,
+        data: group.weekData[this.state.week],
+        backgroundColor: "rgba(" + colors[i] + ", 0.2)",
+        borderColor: "rgba(" + colors[i] + ", 1)",
+        borderWidth: 2,
+        lineTension: 0.1,
+        fill: true
+      });
+    });
+    dataset.push({
+      label: "Recommended amount of hours",
+      data: [10, 10, 10, 10, 10, 10],
+      pointRadius: 0,
+      backgroundColor: "rgba(255, 99, 132, 0.2)",
+      borderColor: "rgba(255, 99, 132, 0.2)",
+      borderDash: [5, 3],
+      borderWidth: 2,
+      lineTension: 0.4,
+      fill: "origin"
+    });
+    return dataset;
   };
 
-  getGroupHours = (group) => {
-      var weeks = {
-          45: [0, 0, 0, 0, 0, 0],
-          46: [0, 0, 0, 0, 0, 0],
-          47: [0, 0, 0, 0, 0, 0],
-          48: [0, 0, 0, 0, 0, 0],
-          49: [0, 0, 0, 0, 0, 0]
-      };
+  getGroupWeeks = group => {
+    var weeks = {
+      45: [0, 0, 0, 0, 0, 0],
+      46: [0, 0, 0, 0, 0, 0],
+      47: [0, 0, 0, 0, 0, 0],
+      48: [0, 0, 0, 0, 0, 0],
+      49: [0, 0, 0, 0, 0, 0]
+    };
 
     students.forEach(student => {
-        if (student.group === group) {
-            for (let week in student.weekData) {
-                let hourList = student.weekData[week];
-                for (var i = 0; i < student.weekData[week].length; i++) {
-                    weeks[week][i] += hourList[i];
-            }
-            }
+      if (student.group === group) {
+        for (let week in student.weekData) {
+          let hourList = student.weekData[week];
+          for (var i = 0; i < student.weekData[week].length; i++) {
+            weeks[week][i] += hourList[i];
+          }
         }
+      }
     });
     for (let week in weeks) {
-        for (var i = 0; i < weeks[week].length; i++)
+      for (var i = 0; i < weeks[week].length; i++)
         weeks[week][i] = Math.round((weeks[week][i] / 6) * 10) / 10;
     }
+    return weeks;
   };
 
   test = e => {
@@ -76,28 +113,7 @@ export default class GroupManagement extends Component {
         "Design Critique",
         "Presentation"
       ],
-      datasets: [
-        {
-          label: groupA.name,
-          data: groupA.weekData[this.state.week],
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
-          borderColor: "rgba(54, 162, 235, 1)",
-          borderWidth: 2,
-          lineTension: 0.1,
-          fill: true
-        },
-        {
-          label: "Recommended amount of hours",
-          data: [10, 10, 10, 10, 10, 10],
-          pointRadius: 0,
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          borderColor: "rgba(255, 99, 132, 0.2)",
-          borderDash: [5, 3],
-          borderWidth: 2,
-          lineTension: 0.4,
-          fill: "origin"
-        }
-      ]
+      datasets: this.state.data
     };
 
     var options = {
